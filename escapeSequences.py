f949d755a5ce11e2b24a876e91e5135cff0c4c47
jlcatonjr <jlcatonjr@gmail.com>
2018-09-15 10:46:27 -0500

    2nd commit for push

---
+++ escapeSequences.py
@@ -0,0 +1,12 @@
+#escapeSequences.py
+singleQuotes = 'We may use \'single quotes\' in single quotes.'
+doubleQuotes = "Or we may use \"double quotes\" in double quotes."
+readBackslash = "We may use two backslashes to print a single backslash: \\"
+lines = "Every\nword\nis\na\nnew\nline"
+newLineAndTab = "We may start a new line \n\tand use tab for a hanging indent"
+
+print(singleQuotes)
+print(doubleQuotes)
+print(readBackslash)
+print(lines)
+print(newLineAndTab)
---
+++ replace.py
@@ -0,0 +1,6 @@
+#replace.py
+spaces = "    Look at all the spaces in the text!    "
+print("no spaces removed:\n", spaces)
+
+removeAllSpaces = spaces.replace(" ", "")
+print("Remove all spaces:\n" + removeAllSpaces)
