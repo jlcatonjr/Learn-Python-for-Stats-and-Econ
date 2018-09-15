7b7b4b343b47f299af8e4e3c42f6c2733c01a2f6
jlcatonjr <jlcatonjr@gmail.com>
2018-09-15 11:56:28 -0500

    Homework 1

---
+++ Chapter 1/homework1.py
@@ -0,0 +1,20 @@
+#homework1.py
+
+""" 
+1. Record your name in an object (x = ...) and a description of yourself in 
+another object. 
+
+2. Raise 2 to the 4th power. Save the value in an object named twoToTheFourth. 
+Then create a new value that sums the value twice. Create another variable that 
+saves the value of twoToTheFourth as a string. Just as with the numeric value, 
+sum the string object twice.
+
+3. Find the list of escape sequences in section 2.4:
+    https://docs.python.org/3/reference/lexical_analysis.html#literals
+    create a string that uses at least 4 different escape sequences.
+    
+4. Add an integer and a float together. Is the final number an integer or a float?
+
+5. Add 2 ** 1024 + 1.5. What is the outcome? Why?
+"""
+
