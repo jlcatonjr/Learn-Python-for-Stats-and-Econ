0305324c7c1546c25cce959227519afbbbecc414
jlcatonjr <jlcatonjr@gmail.com>
2018-09-15 10:48:24 -0500

    organize chapters by folder

---
+++ Chapter 1/quotesInQuotes.py
@@ -0,0 +1,5 @@
+#quotesInQuotes.py
+singleInDouble = "We may use 'single quotes' within double quotes"
+doubleInSingle = 'We may use "double quotes" within single quotes'
+print(singleInDouble)
+print(doubleInSingle)
---
+++ Chapter 1/stringCaps.py
@@ -0,0 +1,9 @@
+#stringCaps.py
+msg = "john nash"
+print(msg)
+
+#ALL CAPS
+print(msg.upper())
+
+#First Letter Of Each Word Capitalized
+print(msg.title())
