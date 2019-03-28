1873dba54d2b32a08a2b87936b3763d0e8da6e3e
jlcatonjr <jlcatonjr@gmail.com>
2019-03-28 10:41:51 -0500

    Add Homework 6

---
+++ homework6.py
@@ -0,0 +1,19 @@
+#1.	Run an OLS regression using a different set of data. Use the regression 
+#class created in this chapter. Print the results.
+
+#2.	Create scatter plots of the observation and predicted values as demonstrated at the end of this chapter.
+
+#3.	Use the numpy libraries log function to log some or all value in your data. 
+#Print the columns of data that have been logged. (hint: pass the appropriate 
+#list of keys to the dataframe, data[[key1,key2,key3…]])
+
+#4.	Run the same regression again. Print the results. How has the significance 
+#changed?
+
+#5.	Plot the new results using scatter plots as in question 2.
+
+#6.	Create 2 unique visualizations of the results using matplotlib 
+#(e.g., time series predicted values and observations, plots with more than 
+#2 variables represented such as 3D plane or changing size of dots, a plot 
+#comparing results of the logged and unlogged regression, etc…). For 
+#visualization ideas see: https://matplotlib.org/examples/.
