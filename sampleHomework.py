86e5f58ade1c78af71bed2f0b12cd240e8b7d328
jlcatonjr <jlcatonjr@gmail.com>
2019-08-28 13:46:35 -0500

    Add sampleHomework.py

---
+++ sampleHomework.py
@@ -0,0 +1,22 @@
+#sampleHomeworkProblem.py
+print("""
+1.	Save your name as 3 or more separate objects (3 separate variables) and 
+concatenate those objects and save the result in a new variable. Print the result:""")
+
+
+print("""
+SCRIPT:
+first_name = "James"
+middle_name = "Lee"
+last_name = "Caton"
+full_name = first_name + " "  + middle_name + " " + last_name
+
+RESULT:
+""")
+    
+first_name = "James"
+middle_name = "Lee"
+last_name = "Caton"
+full_name = first_name + " "  + middle_name + " " + last_name
+
+print("Full Name:", full_name)
