a74cdaf61cc78966567fe93bae209d85c24007ee
jlcatonjr <jlcatonjr@gmail.com>
2019-09-06 12:17:11 -0500

    updated homework layout with example for #3

---
+++ homeworkLayout.py
@@ -0,0 +1,143 @@
+import numpy as np
+import decimal
+import string
+#homeworkLayout.py
+print("""
+James Caton
+ECON 411 / 611
+Homework
+""")
+
+print("""
+1. Record your name in an object (x = ...) and a description of yourself in 
+another object. 
+""")
+
+
+
+print("""
+SCRIPT:
+
+RESULT:
+""")
+
+    
+print("""
+Explanation:
+
+""")    
+
+
+    
+    
+
+print("""
+2. Raise 2 to the 4th power. Save the value as an object named 
+two_to_the_fourth. Then create a new value that sums the value twice. 
+Create another variable that saves the value of twoToTheFourth as a string. 
+Just as with the numeric value, sum the string object twice.
+""")
+
+print("""
+SCRIPT:
+
+    
+    
+RESULT:
+""")
+
+print("""
+Explanation:
+
+""")    
+    
+    
+    
+    
+print("""
+3. Find the list of escape sequences in section 2.4: 
+https://docs.python.org/3/reference/lexical_analysis.html#literals 
+create a string that uses at least 4 different escape sequences.
+""")
+
+tab_word = "I use the \t tab escape sequence"
+new_line = "It was boring,\nso I made a new line"
+escape_quote = "How \"convenient\" it is to include scare quote"
+backslash = "And who knew that \"\\\" did so much work!"
+
+    
+print("""
+SCRIPT:
+tab_word = "I use the \\t tab escape sequence"
+new_line = "It was boring,\\nso I made a new line"
+escape_quote = "How \\"convenient\\" it is to include scare quote"
+backslash = "And who knew that \\"\\\\\\" did so much work!"
+
+    
+RESULT:
+""")  
+print(tab_word, new_line, escape_quote, backslash, sep="\n")
+    
+    
+print("""
+Explanation:
+I created four different objects using different escape sequences. Getting the
+result to print correctly under SCRIPT was something of a challenge as I had to 
+backslashes in front of every backslash. This might have been easier if I had
+used ctrl-r and replaced each (single) \ with (double) \\
+""")    
+
+
+
+
+
+
+print("""    
+4. Add an integer and a float together. Is the final number an integer or a 
+float?
+""")
+
+
+print("""
+SCRIPT:
+
+    
+    
+RESULT:
+""")
+
+
+    
+    
+    
+    
+    
+    
+    
+print("""
+5. Add 2 ** 1024 + 1.5. What is the outcome? Why? hint: Try adding .5, 1, and  
+1.1 instead of 1.5.
+""")
+val = 2 ** 1024
+# 2 ** 1024 + 1.0 is to large of a value to fit in the float datatype
+#new_val0 = 2**1024 + 1.0 
+#similar error as above
+new_val1 = decimal.Decimal(val) + decimal.Decimal(.5)
+new_val2 = val + 1
+#same float error
+#new_val3 = val + 1.1
+
+print("""
+SCRIPT:
+
+    
+    
+RESULT:
+""")
+print(val)
+print(new_val1)
+print("""
+EXPLANATION: 
+""")
+    
+    
