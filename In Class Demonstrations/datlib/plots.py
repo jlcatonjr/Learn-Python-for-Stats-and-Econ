ef1723670f8a3a1497ee93d696d9146512ba7569
jlcatonjr <jlcatonjr@gmail.com>
2021-10-28 12:03:39 -0500

    update class projects/demonstration

--- In Class Demonstrations/datlib/plots.py
+++ In Class Demonstrations/datlib/plots.py
@@ -87,7 +87,51 @@ def plot_stacked_lines(df, plot_vars, linewidth = 1,
     if title != False:
         plt.title(title)
     
-    
+def corr_matrix_heatmap(df, save_fig = False, pp = None, title = "Correlation"):  
+    #Create a figure to visualize a corr matrix  
+    fig, ax = plt.subplots(figsize=(20,20))  
+    # use ax.imshow() to create a heatmap of correlation values  
+    # seismic mapping shows negative values as blue and positive values as red  
+    im = ax.imshow(df, norm = plt.cm.colors.Normalize(-1,1), cmap = "seismic")  
+    # create a list of labels, stacking each word in a label by replacing " "  
+    # with "\n"  
+    labels = df.keys()  
+    num_vars = len(labels)  
+    tick_labels = [lab.replace(" ", "\n") for lab in labels]  
+    # adjust font size according to the number of variables visualized  
+    tick_font_size = 120 / num_vars  
+    val_font_size = 200 / num_vars  
+    plt.rcParams.update({'font.size': tick_font_size}) 
+    # prepare space for label of each column  
+    x_ticks = np.arange(num_vars)  
+    # select labels and rotate them 90 degrees so that they are vertical  
+    plt.xticks(x_ticks, tick_labels, fontsize = tick_font_size, rotation = 90)  
+    # prepare space for label of each row  
+    y_ticks = np.arange(len(labels))  
+    # select labels  
+    plt.yticks(y_ticks, tick_labels, fontsize = tick_font_size)  
+    # show values in each tile of the heatmap  
+    for i in range(len(labels)):  
+        for j in range(len(labels)):  
+            text = ax.text(i, j, str(round(df.values[i][j],2)),  
+                           fontsize= val_font_size, ha="center",   
+                           va="center", color = "w")  
+    #Create title with Times New Roman Font  
+    title_font = {"fontname":"Times New Roman"}  
+    plt.title(title, fontsize = 50, **title_font)  
+    #Call scale to show value of colors 
+    cbar = fig.colorbar(im)
+    plt.show()
+    if save_fig:
+        try:
+            os.mkdir("plots")
+        except:
+            pass
+        plt.savefig("plots/" + str(list(df.keys())).replace("[", "").replace("]","")[:40] + " corrMatrix.png",
+            bbox_inches = "tight")
+
+        if pp != None: pp.savefig(fig, bbox_inches="tight")
+    plt.close()    
     
     
     
